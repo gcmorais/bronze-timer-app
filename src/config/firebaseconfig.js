2335f76a921f6ce3fe2fcec520df3b07cbb74235
Guilherme Morais <guilhermeclarodemorais@gmail.com>
2023-12-14 18:48:03 -0300

    feat: performing first query on the firebase database

---
+++ src/config/firebaseconfig.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDAGH-KuainpPzTEv8qIaa7lmKd-_DsFCw",
+  authDomain: "firebelaebronze.firebaseapp.com",
+  projectId: "firebelaebronze",
+  storageBucket: "firebelaebronze.appspot.com",
+  messagingSenderId: "127880012172",
+  appId: "1:127880012172:web:0745d347923d6ffe6336a9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const database = getFirestore(app);
+
+export default database;
