3496348f4c6d05c7b64ab501f03be4b976c44308
Guilherme Morais <guilhermeclarodemorais@gmail.com>
2023-12-19 17:15:45 -0300

    delete all files and reconfig project

--- App.js
+++ App.js
@@ -1,51 +1,20 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-
-import Task from "./src/pages/Task";
-import NewTask from "./src/pages/NewTask";
-import Details from "./src/pages/Details";
-import Login from "./src/pages/Login";
-
-const Stack = createStackNavigator();
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="Task"
-          component={Task}
-          options={{
-            headerTintColor: "#F92E6A",
-            headerTitleAlign: "left",
-            headerTitle: "Bela & Bronze",
-          }}
-        />
-        <Stack.Screen
-          name="New Task"
-          component={NewTask}
-          options={{
-            headerTintColor: "#F92E6A",
-            headerBackTitle: "voltar",
-          }}
-        />
-        <Stack.Screen
-          name="Details"
-          component={Details}
-          options={{
-            headerTintColor: "#F92E6A",
-            headerBackTitle: "voltar",
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
